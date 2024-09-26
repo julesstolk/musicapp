64c605a5c0f9f55197321c742243bf7e5cdad294
InnocentCow <julesstolk@gmail.com>
2024-09-26 18:50:05 +0200

    app now asks for audio file permissions

--- app/src/main/java/com/blyss2idk/musicapp/MainActivity.kt
+++ app/src/main/java/com/blyss2idk/musicapp/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.blyss2idk.musicapp
 
-import android.app.Activity
+import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.ActivityResultCaller
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -33,7 +33,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.blyss2idk.musicapp.ui.theme.MusicappMain
-import kotlin.system.exitProcess
 
 class MainActivity : ComponentActivity() {
 
@@ -47,18 +46,48 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        if (ContextCompat.checkSelfPermission(this, "READ_EXTERNAL_AUDIO") != PackageManager.PERMISSION_GRANTED) {
-            val activityresultcallback = ARC()
+        checkPermissions()
+
+    }
+
+
+    // Ask for permissions for reading audio media
+    private fun checkPermissions() {
+        val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission())
+            { isGranted: Boolean ->
+                if (!isGranted) {
+                    // Permission not granted, show snackbar that says that app is unusable
+                    // and/or close app
+                    // NotifyNoPermissionSnackbar()
+                }
+            }
+        // Check android version to support both older as newer android versions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // Ask permission if not granted, else do nothing
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_MEDIA_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                    // If permission not granted, ask for permission
+                    requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_AUDIO)
+            }
+        }
+        else {
+            // Ask permission READ_EXTERNAL_STORAGE instead, for older android versions
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                // Ask permission if not granted, else do nothing
+                if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    // If permission not granted, ask for permission
+                    requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }
         }
     }
 
-    class ARC : ActivityResultCallback<Boolean> {
-        override fun onActivityResult(result: Boolean) {
-            if (!result) {
-                exitProcess(0)
-            }
-        }
-
+    @Composable
+    fun NotifyNoPermissionSnackbar(permission: String) {
+        // to implement
     }
 
     @Preview
