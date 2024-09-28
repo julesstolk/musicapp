2cfeca30a7ee3985d0776eb19dc95984871c8029
InnocentCow <julesstolk@gmail.com>
2024-09-29 01:15:12 +0200

    SearchManager now returns mutable list of all tracks found

--- app/src/main/java/com/blyss2idk/musicapp/SearchManager.kt
+++ app/src/main/java/com/blyss2idk/musicapp/SearchManager.kt
@@ -1,35 +1,69 @@
 package com.blyss2idk.musicapp
 
-import java.io.File
+import android.content.Context
+import android.database.Cursor
+import android.os.Build
 import android.provider.MediaStore
 import android.util.Log
-import androidx.activity.ComponentActivity
 
-class SearchManager(private var context: ComponentActivity) {
+class SearchManager(private var context: Context) {
 
-    // Change type
-    var knownFiles = mutableListOf<File>()
+    fun cursorToTrackClass(cursor: Cursor): Track {
+        val id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
+        val displayName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
+        val title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
+        val artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+        val duration = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
+        val filePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
 
-    fun search(query: String) {
+        return Track(id, displayName, title, artist, duration, filePath)
+    }
+
+    fun search(query: String): MutableList<Track> {
+
+        val outputTracks = mutableListOf<Track>()
 
         val projection = arrayOf(
             MediaStore.Audio.Media._ID,            // ID of the audio file
             MediaStore.Audio.Media.DISPLAY_NAME,   // Name of the audio file
-
-            MediaStore.Audio.Media.DATA            // Full file path to audio file
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.DURATION,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Audio.Media.RELATIVE_PATH
+            } else {
+                MediaStore.Audio.Media.DATA
+            }
         )
 
-        context.contentResolver.query(
-            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-            projection,
-            null,
-            null,
-            null
-        )?.use { cursor ->
-            Log.i("FILETEST", "yaya2")
-            while (cursor.moveToNext()) {
-                Log.i("FILETEST", cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)))
+        try {
+            context.contentResolver.query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                null,
+                null,
+                null
+            )?.use { cursor ->
+                Log.i("FILETEST", "yaya2")
+                if (cursor.count == 0) {
+                    Log.i("FILETEST", "empty")
+                } else {
+                    Log.i("FILETEST", "not empty")
+                }
+                while (cursor.moveToNext()) {
+                    Log.i(
+                        "FILETEST",
+                        cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
+                    )
+
+                    outputTracks.add(cursorToTrackClass(cursor))
+
+                }
             }
+        } catch (e: IllegalArgumentException) {
+            Log.e("FILETEST", "Query failed: ", e)
         }
+
+        return outputTracks
     }
 }
