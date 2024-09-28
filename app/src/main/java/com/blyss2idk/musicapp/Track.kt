18e4061d4adb038a2c4b22c2eab695ffe0706e9f
InnocentCow <julesstolk@gmail.com>
2024-09-29 01:04:55 +0200

    changed Track to data class

--- app/src/main/java/com/blyss2idk/musicapp/Track.kt
+++ app/src/main/java/com/blyss2idk/musicapp/Track.kt
@@ -1,15 +1,30 @@
 package com.blyss2idk.musicapp
 
+import android.database.Cursor
 import android.graphics.Bitmap
+import android.provider.MediaStore
 
-class Track(val fileName: String,
-            val fileId: Int,
-            val filePath: String,
-            val duration: Int,
-            val title: String?,
-            val artist: String?) {
+data class Track(val fileId: Int,
+                 val fileName: String,
+                 val title: String?,
+                 val artist: String?,
+                 val duration: Int,
+                 val filePath: String) {
 
     var hasMetadata = false
     var useMetadata = false
     lateinit var cover: Bitmap
+
+    fun deserializeCursor(cursor: Cursor) {
+        val fileId = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
+        val fileName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
+        val title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
+        val artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+        val duration = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
+        val filePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
+
+        if (title != null && artist != null) {
+            useMetadata = true
+        }
+    }
 }
