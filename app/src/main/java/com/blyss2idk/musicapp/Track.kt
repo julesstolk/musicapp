ccadef1cc28f2bde17d8a6595a48448af506c296
InnocentCow <julesstolk@gmail.com>
2024-09-29 01:07:36 +0200

    updated deserializeCursor to return a Track instance

--- app/src/main/java/com/blyss2idk/musicapp/Track.kt
+++ app/src/main/java/com/blyss2idk/musicapp/Track.kt
@@ -4,18 +4,18 @@ import android.database.Cursor
 import android.graphics.Bitmap
 import android.provider.MediaStore
 
-data class Track(val fileId: Int,
+data class Track(val fileId: Long,
                  val fileName: String,
                  val title: String?,
                  val artist: String?,
-                 val duration: Int,
+                 val duration: Long,
                  val filePath: String) {
 
     var hasMetadata = false
     var useMetadata = false
     lateinit var cover: Bitmap
 
-    fun deserializeCursor(cursor: Cursor) {
+    fun deserializeCursor(cursor: Cursor): Track {
         val fileId = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
         val fileName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
         val title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
@@ -26,5 +26,7 @@ data class Track(val fileId: Int,
         if (title != null && artist != null) {
             useMetadata = true
         }
+
+        return Track(fileId, fileName, title, artist, duration, filePath)
     }
 }
