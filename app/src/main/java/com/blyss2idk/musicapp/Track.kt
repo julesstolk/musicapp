cb7d31cd2c8874ad800486a2eb49365bd054aa23
InnocentCow <julesstolk@gmail.com>
2024-09-29 01:12:44 +0200

    properly made Track a data class

--- app/src/main/java/com/blyss2idk/musicapp/Track.kt
+++ app/src/main/java/com/blyss2idk/musicapp/Track.kt
@@ -1,8 +1,6 @@
 package com.blyss2idk.musicapp
 
-import android.database.Cursor
 import android.graphics.Bitmap
-import android.provider.MediaStore
 
 data class Track(val fileId: Long,
                  val fileName: String,
@@ -15,18 +13,9 @@ data class Track(val fileId: Long,
     var useMetadata = false
     lateinit var cover: Bitmap
 
-    fun deserializeCursor(cursor: Cursor): Track {
-        val fileId = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
-        val fileName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
-        val title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
-        val artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
-        val duration = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
-        val filePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
-
+    init {
         if (title != null && artist != null) {
-            useMetadata = true
+            hasMetadata = true
         }
-
-        return Track(fileId, fileName, title, artist, duration, filePath)
     }
 }
