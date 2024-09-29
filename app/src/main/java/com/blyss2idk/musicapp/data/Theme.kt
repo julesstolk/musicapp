4bf15d9ca81665a1e9960005eaecf3257326113e
InnocentCow <julesstolk@gmail.com>
2024-09-29 23:13:53 +0200

    expanded Theme options

--- app/src/main/java/com/blyss2idk/musicapp/data/Theme.kt
+++ app/src/main/java/com/blyss2idk/musicapp/data/Theme.kt
@@ -5,7 +5,14 @@ import androidx.compose.ui.graphics.Color
 data class Theme(
     val title: String,
     val backgroundColor: Color,
-    val primaryColor: Color,
-    val textColor: Color
-) {
-}
+    val textColor: Color,
+    val tabsColor: Color,
+    val tabsHorizontal: Int = 1,
+    val tabsVertical: Int = 10,
+    val tabsSize: Int = 70,
+    val tabsSpacing: Int = 7,
+    val searchBarColor: Color = tabsColor,
+    val tabRoundedCornerShape: Int = 7,
+    val searchButtonTextSpacing: Int = 10,
+    val backGroundImage: Int? = null,
+)
