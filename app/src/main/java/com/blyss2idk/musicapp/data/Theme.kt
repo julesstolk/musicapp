b120e6e73647dbd2793843ee97813fa5a167f14a
InnocentCow <julesstolk@gmail.com>
2024-09-29 20:16:52 +0200

    created Theme.kt class

---
+++ app/src/main/java/com/blyss2idk/musicapp/data/Theme.kt
@@ -0,0 +1,11 @@
+package com.blyss2idk.musicapp.data
+
+import androidx.compose.ui.graphics.Color
+
+data class Theme(
+    val title: String,
+    val backgroundColor: Color,
+    val primaryColor: Color,
+    val textColor: Color
+) {
+}
