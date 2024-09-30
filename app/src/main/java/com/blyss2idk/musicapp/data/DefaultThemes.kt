00b61bf3751709575a73ef6b2dfabd6133c0e529
jules <j.b.ij.stolk@student.utwente.nl>
2024-09-30 14:46:47 +0200

    created DefaultThemes.kt and dark theme

---
+++ app/src/main/java/com/blyss2idk/musicapp/data/DefaultThemes.kt
@@ -0,0 +1,8 @@
+package com.blyss2idk.musicapp.data
+
+import androidx.compose.ui.graphics.Color
+
+class DefaultThemes {
+
+    val darkTheme = Theme("dark theme", Color.Black, Color.White, Color.Gray, 2)
+}
