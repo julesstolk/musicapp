4385eea4c531434648fea713c853d9998ab124d1
InnocentCow <julesstolk@gmail.com>
2024-09-30 22:10:15 +0200

    added background images

--- app/src/main/java/com/blyss2idk/musicapp/data/DefaultThemes.kt
+++ app/src/main/java/com/blyss2idk/musicapp/data/DefaultThemes.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 class DefaultThemes {
     companion object {
-        val darkTheme = Theme(title = "dark theme", backgroundColor = Color.Black, textColor = Color.White, tabColor = Color.Gray, tabAlpha = 0.8f, tabsHorizontal = 2, tabRoundedCornerShape = 2, tabPadding = 2)
+        val darkTheme = Theme(title = "dark minimal", backgroundColor = Color.Black, textColor = Color.White, tabColor = Color.Gray, tabAlpha = 1f, tabsHorizontal = 2, tabRoundedCornerShape = 2, tabPadding = 2)
+        val darkTheme2 = Theme(title = "dark", backgroundColor = Color.Black, textColor = Color.White, tabColor = Color.Gray.copy(alpha=0.5f), tabAlpha = 0.9f, tabsHorizontal = 2, tabRoundedCornerShape = 2, tabPadding = 2, backgroundImage = "bg.png")
     }
 }
