096702c88d84f01638641070c24d27589031cc47
InnocentCow <julesstolk@gmail.com>
2024-09-30 22:46:37 +0200

    added textSize

--- app/src/main/java/com/blyss2idk/musicapp/data/DefaultThemes.kt
+++ app/src/main/java/com/blyss2idk/musicapp/data/DefaultThemes.kt
@@ -1,10 +1,11 @@
 package com.blyss2idk.musicapp.data
 
 import androidx.compose.ui.graphics.Color
+import com.blyss2idk.musicapp.R
 
 class DefaultThemes {
     companion object {
         val darkTheme = Theme(title = "dark minimal", backgroundColor = Color.Black, textColor = Color.White, tabColor = Color.Gray, tabAlpha = 1f, tabsHorizontal = 2, tabRoundedCornerShape = 2, tabPadding = 2)
-        val darkTheme2 = Theme(title = "dark", backgroundColor = Color.Black, textColor = Color.White, tabColor = Color.Gray.copy(alpha=0.5f), tabAlpha = 0.9f, tabsHorizontal = 2, tabRoundedCornerShape = 2, tabPadding = 2, backgroundImage = "bg.png")
+        val darkTheme2 = Theme(title = "dark", backgroundColor = Color.Black, textColor = Color.White, tabColor = Color.Gray.copy(alpha=0.5f), tabAlpha = 0.9f, tabsHorizontal = 2, tabRoundedCornerShape = 2, tabPadding = 2, backgroundImage = R.drawable.bg, tabSizeVertical = 50, textSize = 20f)
     }
 }
