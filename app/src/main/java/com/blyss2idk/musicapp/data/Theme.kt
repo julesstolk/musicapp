90f8f09b803511721731d6c29851c253a5ab9dd5
jules <j.b.ij.stolk@student.utwente.nl>
2024-09-30 15:11:16 +0200

    changed some names and added extra settings

--- app/src/main/java/com/blyss2idk/musicapp/data/Theme.kt
+++ app/src/main/java/com/blyss2idk/musicapp/data/Theme.kt
@@ -3,15 +3,16 @@ package com.blyss2idk.musicapp.data
 import androidx.compose.ui.graphics.Color
 
 data class Theme(
+    // no tabSizeHorizontal; fill max width
     val title: String,
     val backgroundColor: Color,
     val textColor: Color,
-    val tabsColor: Color,
+    val tabColor: Color,
     val tabsHorizontal: Int = 1,
     val tabsVertical: Int = 10,
-    val tabsSize: Int = 70,
-    val tabsSpacing: Int = 7,
-    val searchBarColor: Color = tabsColor,
+    val tabSizeVertical: Int = 70,
+    val tabPadding: Int = 7,
+    val searchBarColor: Color = tabColor,
     val tabRoundedCornerShape: Int = 7,
     val searchButtonTextSpacing: Int = 10,
     val backGroundImage: Int? = null,
