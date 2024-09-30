55da7be71e84a01a59da143adeb87a8887175436
InnocentCow <julesstolk@gmail.com>
2024-09-30 23:01:16 +0200

    changed tabSize

--- app/src/main/java/com/blyss2idk/musicapp/data/DefaultThemes.kt
+++ app/src/main/java/com/blyss2idk/musicapp/data/DefaultThemes.kt
@@ -6,6 +6,6 @@ import com.blyss2idk.musicapp.R
 class DefaultThemes {
     companion object {
         val darkTheme = Theme(title = "dark minimal", backgroundColor = Color.Black, textColor = Color.White, tabColor = Color.Gray, tabAlpha = 1f, tabsHorizontal = 2, tabRoundedCornerShape = 2, tabPadding = 2)
-        val darkTheme2 = Theme(title = "dark", backgroundColor = Color.Black, textColor = Color.White, tabColor = Color.Gray.copy(alpha=0.5f), tabAlpha = 0.9f, tabsHorizontal = 2, tabRoundedCornerShape = 2, tabPadding = 2, backgroundImage = R.drawable.bg, tabSizeVertical = 50, textSize = 20f)
+        val darkTheme2 = Theme(title = "dark", backgroundColor = Color.Black, textColor = Color.White, tabColor = Color.Gray.copy(alpha=0.5f), tabAlpha = 0.9f, tabsHorizontal = 2, tabRoundedCornerShape = 2, tabPadding = 2, backgroundImage = R.drawable.bg, tabSizeVertical = 60, textSize = 20f)
     }
 }
