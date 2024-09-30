de50be106235ebd177fa5fdd7920bb20c6d694ac
jules <j.b.ij.stolk@student.utwente.nl>
2024-09-30 14:49:47 +0200

    moved dark theme into companion object

--- app/src/main/java/com/blyss2idk/musicapp/data/DefaultThemes.kt
+++ app/src/main/java/com/blyss2idk/musicapp/data/DefaultThemes.kt
@@ -3,6 +3,7 @@ package com.blyss2idk.musicapp.data
 import androidx.compose.ui.graphics.Color
 
 class DefaultThemes {
-
-    val darkTheme = Theme("dark theme", Color.Black, Color.White, Color.Gray, 2)
+    companion object {
+        val darkTheme = Theme("dark theme", Color.Black, Color.White, Color.Gray, 2)
+    }
 }
