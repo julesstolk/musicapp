57931c1f629a1ad59ac10fe2738321a301170d86
jules <j.b.ij.stolk@student.utwente.nl>
2024-10-01 11:41:26 +0200

    moved icons into TabType

--- app/src/main/java/com/blyss2idk/musicapp/data/TabType.kt
+++ app/src/main/java/com/blyss2idk/musicapp/data/TabType.kt
@@ -1,5 +1,18 @@
 package com.blyss2idk.musicapp.data
 
+import com.blyss2idk.musicapp.R
+
 enum class TabType {
-    SONG, PLAYLIST, PREFERENCE, EXCEPTION
+    SONG {
+        fun icon() = R.drawable.baseline_music_note_24
+    },
+    PLAYLIST {
+        fun icon() = R.drawable.baseline_library_music_24
+    },
+    PREFERENCE {
+        fun icon() = R.drawable.baseline_settings_suggest_24
+    },
+    EXCEPTION {
+        fun icon() = R.drawable.baseline_cancel_24
+    }
 }
