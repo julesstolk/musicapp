ef23b6670884db9575d638514c7060710c5ddd91
Stolk, J.B.IJ. (Jules, Student B-TCS) <j.b.ij.stolk@student.utwente.nl>
2025-04-15 16:42:42 +0200

    changes 🤣🤣🤣🤣😂😂🤣😂😃😃😃 !!  !!!! ! !!!   !!!  ! ! ! ! ! !!!!!!!!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    kotlin("plugin.serialization") version "2.1.20"
 }
 
 android {
@@ -59,6 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
